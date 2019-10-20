293dc18fbf72877da3934f44a3d5405d1fd9a211
BuildTools <unconfigured@null.spigotmc.org>
2019-10-19 20:26:08 -0700

    Added support to write to JSON

--- main.cpp
+++ main.cpp
@@ -5,17 +5,21 @@
 int main(int argc, char** argv) {
     if(argc != 2) {
         printf("Usage: %s <parameters>\n", argv[0]);
-
-    } else {
-
-        Record sample = read_json("data/sample-db.json");
-
-        Record& members = sample["members"];
-        for(int i = 0; i < members.size(); ++i) {
-            printf("Name: %s\n", members[i]["name"].asString().c_str());
-        }
-
-        return 0;
+        return -1;
     }
-    return -1;
+
+    //example of reading a json file
+    Record sample = read_json("data/sample-db.json");
+
+    Record& members = sample["members"];
+    for(int i = 0; i < members.size(); ++i) {
+        printf("Name: %s\n", members[i]["name"].asString().c_str());
+    }
+
+    //modifying a json value
+    members[1]["name"] = "test";
+    //example of writing to a json file
+    write_json("data/test.json", sample);
+
+    return 0;
 }
