391c9afe7169d512246523f4d6e9449b91d1387a
Corbin Stark <starkcorbin@gmail.com>
2019-11-25 19:34:34 -0800

    Updated main.cpp to display manager and provider options

--- main.cpp
+++ main.cpp
@@ -18,13 +18,51 @@ int main(int argc, char** argv) {
             cout << "Invalid input. Type 'm' for the manager terminal or 'p' for the provider terminal." << endl;
     }
 
+    char choice = 0;
     if(res == 'm') {
-        cout << "Welcome to the manager terminal" << endl;
-        //Someone does this
+        do {
+            cout << "Welcome to the manager terminal. Please select an option." << endl;
+            cout << "(a) Add a new provider" << endl;
+            cout << "(b) Add a new member" << endl;
+            cout << "(c) Edit a provider or member" << endl;
+            cout << "(d) Delete a provider or member" << endl;
+            cout << "(x) Exit" << endl;
+            cin >> choice;
+        }
+        while(choice != 'a' && choice != 'b' && choice != 'c' && choice != 'd' && choice !='x');
+
+        switch(choice) {
+            case 'a':
+                break;
+            case 'b':
+                break;
+            case 'c':
+                break;
+            case 'd':
+                break;
+            case 'x':
+                break;
+        }
     }
+    
     if(res == 'p') {
-        cout << "Welcome to the provider terminal" << endl;
-        //Someone does this
+        do {
+            cout << "Welcome to the provider terminal" << endl;
+            cout << "(a) View the provider directory" << endl;
+            cout << "(b) Provide service" << endl;
+            cout << "(x) Exit" << endl;
+            cin >> choice;
+        }
+        while(choice != 'a' && choice != 'b' && choice != 'x');
+        
+        switch(choice) {
+            case 'a':
+                break;
+            case 'b':
+                break;
+            case 'x':
+                break;
+        }
     }
 
     return 0;
