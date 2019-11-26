8906c9d8c6ad35d264a128fecd00a76d78ba2198
Corbin Stark <starkcorbin@gmail.com>
2019-11-25 19:12:11 -0800

    Changed main.cpp to have real code instead of test code

--- main.cpp
+++ main.cpp
@@ -2,18 +2,30 @@
  * Created by Bela, Cody Potter, Corbin Stark, Dan Scott, Forest Pearson, Max VanRaden
  */
 
-#define CATCH_CONFIG_RUNNER
-
 #include <iostream>
-#include <catch.hpp>    //Unit Testing library include
-
 #include "Database.h"
+using namespace std;
 
 int main(int argc, char** argv) {
-
     Database database("data/sample-db.json");
 
-    int result = Catch::Session().run( argc, argv );
+    cout << "Welcome to the ChocAn Simulator." << endl;
+    cout << "Would you like to simulate the (m)anager terminal or the (p)rovider terminal?" << endl;
+    char res = 0;
+    while(res != 'm' && res != 'p') {
+        cin >> res;
+        if(res != 'm' && res != 'p')
+            cout << "Invalid input. Type 'm' for the manager terminal or 'p' for the provider terminal." << endl;
+    }
 
-    return result;
+    if(res == 'm') {
+        cout << "Welcome to the manager terminal" << endl;
+        //Someone does this
+    }
+    if(res == 'p') {
+        cout << "Welcome to the provider terminal" << endl;
+        //Someone does this
+    }
+
+    return 0;
 }
--- tests/testing_the_test_library.cpp
+++ tests/testing_the_test_library.cpp
@@ -10,10 +10,10 @@
 
 int Factorial(int number) {
     //this line fails the unit test
-    //return number <= 1 ? number : Factorial(number - 1) * number;
+    return number <= 1 ? number : Factorial(number - 1) * number;
 
     //this line will pass the unit test
-    return number <= 1 ? 1 : Factorial(number - 1) * number;
+    //return number <= 1 ? 1 : Factorial(number - 1) * number;
 }
 
 TEST_CASE("Factorial of 0 is 1 (fail)", "[single-file]") {
