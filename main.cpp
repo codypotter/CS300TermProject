f89a5cb8b05a5f73df253cf822d7614d5b4441db
DanielDScott <danieldscott90@gmail.com>
2019-11-30 13:43:28 -0800

    Provider terminal shit and headers

--- main.cpp
+++ main.cpp
@@ -10,14 +10,34 @@
 
 using namespace std;
 
+void printHeader(char target) {
+	switch(target)
+	{
+		case 'c':
+			cout <<  " __        __   __" << endl;
+			cout << "/  ` |__| /  \\ /  `  /\\  |\\ |" << endl;
+			cout << "\\__, |  | \\__/ \\__, /~~\\ | \\|" << endl << endl;
+			cout << "Welcome to the ChocAn Simulator." << endl;
+			cout << "Would you like to simulate the (m)anager terminal or the (p)rovider terminal?" << endl;
+			break;
+		case 'm':
+			cout << "   __  ___                              ______              _           __" << endl;
+			cout << "  /  |/  /__  ___  ___  ___  ___ ____  /_  __/__ ______ _  (_)__  ___ _/ /" << endl;
+			cout << " / /|_/ / _ `/ _ \\/ _ `/ _ `/ -_) __/   / / / -_) __/  ' \\/ / _ \\/ _ `/ /" << endl;
+			cout << "/_/  /_/\\_,_/_//_/\\_,_/\\_, /\\__/_/     /_/  \\__/_/ /_/_/_/_/_//_/\\_,_/_/" << endl;
+			break;
+		case 'p':
+			cout << " ___             _    _           _____              _           _ " << endl;
+			cout << "| _ \\_ _ _____ _(_)__| |___ _ _  |_   _|__ _ _ _ __ (_)_ _  __ _| |" << endl;
+			cout << "|  _/ '_/ _ \\ V / / _` / -_) '_|   | |/ -_) '_| '  \\| | ' \\/ _` | |" << endl;
+			cout << "|_| |_| \\___/\\_/|_\\__,_\\___|_|     |_|\\___|_| |_|_|_|_|_||_\\__,_|_|" << endl;
+			break;
+	}
+}
+
 int main(int argc, char** argv) {
 	Database database((char*) "data/sample-db.json");
-
-	cout <<  " __        __   __" << endl;
-	cout << "/  ` |__| /  \\ /  `  /\\  |\\ |" << endl;
-	cout << "\\__, |  | \\__/ \\__, /~~\\ | \\|" << endl << endl;
-	cout << "Welcome to the ChocAn Simulator." << endl;
-	cout << "Would you like to simulate the (m)anager terminal or the (p)rovider terminal?" << endl;
+	printHeader('c');
 	char res = 0;
 	while(res != 'm' && res != 'p') {
 		cin >> res;
@@ -27,9 +47,9 @@ int main(int argc, char** argv) {
 
 	char choice = 0;
 	if(res == 'm') {
+		printHeader(res);
 		do {
 			cout << "Welcome to the manager terminal. Please select an option." << endl;
-			cout << "TEST" << endl;
 			cout << "(a) Add a new provider" << endl;
 			cout << "(b) Add a new member" << endl;
 			cout << "(c) Edit a provider or member" << endl;
@@ -54,6 +74,15 @@ int main(int argc, char** argv) {
 	}
 
 	if(res == 'p') {
+		string provID = "";
+		cout << "Please enter provider number: (9 digits)" << endl;
+		cin >> provID;
+		if(database.providers.find(provID) == database.providers.end())
+		{
+			cout << "Invalid provider ID" << endl;
+			return 0;
+		}
+		printHeader(res);
 		do {
 			cout << "Welcome to the provider terminal. Please select an option." << endl;
 			cout << "(a) View the provider directory" << endl;
@@ -66,48 +95,47 @@ int main(int argc, char** argv) {
 		switch(choice) {
 			case 'a':
 				cout << "List of services:" << endl;
+				//THIS PART IS BROKEN AS FUCK!!!!!
 				for(auto pair : database.directory)
 				{
-					string ID = pair.first;
-					ServiceEntry service = pair.second;
-
-					cout << "Service name: " << service.name << endl;
-					cout << "Service code: " << service.id << endl;
-					cout << "Service fee: $" << service.fee << endl;
+					cout << pair.first << " \n" << "name: " << pair.second.name << "\nFee: " << pair.second.fee << endl;
 				}
 				break;
 			case 'b':
 				{
 					string memID = "";
-					cout << "Provide a Service:" << endl;
+					cout << "Provide a Service:" << endl;					
 					cout << "Please enter member ID number: (9 digits)" << endl;
 					cin >> memID;
-					if(database.members.find(memID) == database.members.end()) {
-						cout << "Invalid member ID" << endl;
+					if(database.members.find(memID) == database.members.end())
+					{
+						cout << "Invalid member ID, please try again";
 						break;
 					}
-					if(database.members.at(memID).isValid != true) {
-						cout << "Member status invalid" << endl;
+					if(database.members.at(memID).isValid != true)
+					{
+						cout << "Member status invalid, cannot recieve services" << endl;
 						break;
 					}
-					else {
-						string servCode = "";
-						cout << "Enter a service code: (6 digits)" << endl;
-						cin >> servCode;
-
-						if(database.directory.find(servCode) == database.directory.end()) {
-							cout << "Invalid service code" << endl;
-							break;
-						}
-						else {
-							string currDate = "";
-							string servDate = "";
-							cout << "Enter the current date: (mm/dd/yyyy)" << endl;
-							cin >> currDate;
-							cout << "Enter the expected service date: (mm/dd/yyyy)" << endl;
-							cin >> servDate;
-							//create new service object here with collected data
-						}
+					string servCode = "";
+					//Can't validate service code here
+					cout << "Enter a service code: (6 digits)" << endl;
+					cin >> servCode;
+					if(database.directory.find(servCode) == database.directory.end())
+					{
+						cout << "Invalid service code" << endl;
+						break;
+					}
+					else
+					{
+						string currDate = "";
+						string servDate = "";
+						cout << "Enter the current date: (mm/dd/yyyy)" << endl;
+						cin >> currDate;
+						cout << "Enter the expected service date: (mm/dd/yyyy)" << endl;
+						cin >> servDate;
+						//create new service object here with collected data
+						database.addService(servDate, currDate, memID, provID, servCode, database.directory.at(servCode).fee);
 					}
 				}
 				break;
@@ -115,6 +143,5 @@ int main(int argc, char** argv) {
 				break;
 		}
 	}
-
 	return 0;
 }
